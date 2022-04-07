3cf2e041bbed3580d1f1f4e90661a98cf90cdec4
EatingCookies <js180300@gmail.com>
2022-04-07 16:16:20 -0500

    se agrega el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EstructuraComponent } from './core/components/estructura/estructura.component';
+import { LibroComponent } from './core/components/libro/libro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EstructuraComponent,
+    LibroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/models/libro.ts
@@ -0,0 +1,10 @@
+export class Libro {
+    constructor(
+        public id: number,
+        public name: string,
+        public email: String,
+        public gender: String,
+        public status: string,
+        ){
+    }
+}
